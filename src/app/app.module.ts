7a8f1b747cd939624c6987dd1584a0e7024e3028
Amal <amaldevv@outlook.com>
2021-01-05 12:01:59 +0530

    Created angular version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { SpeakerComponent } from './components/speaker/speaker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SpeakerComponent
   ],
   imports: [
     BrowserModule
